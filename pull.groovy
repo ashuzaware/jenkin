9178201dc04512354d38623aec38cb02b8bfd664
ashuzaware <ashushinde1027@gmail.com>
2024-06-14 16:26:02 +0530

    Update pull.groovy

--- pull.groovy
+++ pull.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('Pull') {
             steps {
-                git 'https://github.com/sharadrathod/studentapp-ui.git'
+                git 'https://github.com/ashuzaware/studentapp-ui-ashu.git'
             }
         }
         stage('Build') {
